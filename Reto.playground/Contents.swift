8c097c3db3b1ae2990949b9b7d1ab8ace7ecb47f
Emiliano Laborda <elaborda@MacBook-Pro-de-Emiliano.local>
2015-12-29 15:33:21 -0300

    adding playgrounds

---
+++ Reto.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+for var i = 0; i <= 100; i++ {
+  
+    var modulo5 = i % 5
+    var modulo2 = i % 2
+    if i >= 30 && i <= 40 {
+        print("\(i)\t Viva Swift!!!")
+    } else if modulo5 == 0 {
+            print("\(i)\t #Bingo!!!")
+        } else if modulo2 == 0 {
+            print("\(i)\t #par!!!")
+        } else {
+            print("\(i)\t #inpar!!!")
+        }
+}
